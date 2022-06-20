b09acfa021cde7827f4539f59f98dd343681de94
Hiago Souza <hiagosouza9@hotmail.com>
2022-06-20 20:43:18 -0300

    delivery_hiago

---
+++ src/main/java/prova2/hiago/HiagoApplication.java
@@ -0,0 +1,13 @@
+package prova2.hiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiagoApplication.class, args);
+	}
+
+}
